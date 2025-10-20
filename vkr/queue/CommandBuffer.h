55facc748672096a6a19f0c16cdaa6dd6010fcbf
Aluminium Rat <AluminiumRat@yandex.ru>
2025-10-20 19:11:25 +0300

    CommandBuffer и CommandPool

---
+++ vkr/queue/CommandBuffer.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+
+#include <vector>
+
+#include <vulkan/vulkan.h>
+
+//#include <mtt/render/CommandQueue/UniformMemoryPool.h>
+//#include <mtt/render/CommandQueue/ScaledVolatileDescriptorPool.h>
+#include <vkr/RefCounter.h>
+#include <vkr/Ref.h>
+
+namespace mt
+{
+  class Device;
+
+  class CommandBuffer : public RefCounter
+  {
+  public:
+    CommandBuffer(VkCommandPool pool,
+                  Device& device,
+                  VkCommandBufferLevel level);
+    CommandBuffer(const CommandBuffer&) = delete;
+    CommandBuffer& operator = (const CommandBuffer&) = delete;
+  protected:
+    virtual ~CommandBuffer();
+
+  public:
+    inline VkCommandBuffer handle() const;
+    inline VkCommandBufferLevel level() const;
+    inline void lockResource(RefCounter& resource);
+    inline void releaseResources();
+    //inline UniformMemoryPool& uniformMemoryPool() noexcept;
+    //inline ScaledVolatileDescriptorPool& volatileDescriptorPool() noexcept;
+
+  private:
+    void _cleanup() noexcept;
+
+  private:
+    VkCommandBuffer _handle;
+    VkCommandPool _pool;
+    Device& _device;
+    VkCommandBufferLevel _level;
+
+    std::vector<RefCounterReference> _lockedResources;
+
+    //UniformMemoryPool _uniformMemoryPool;
+    //ScaledVolatileDescriptorPool _volatileDescriptorPool;
+  };
+
+  inline VkCommandBuffer CommandBuffer::handle() const
+  {
+    return _handle;
+  }
+
+  inline VkCommandBufferLevel CommandBuffer::level() const
+  {
+    return _level;
+  }
+
+  inline void CommandBuffer::lockResource(RefCounter& resource)
+  {
+    _lockedResources.push_back(RefCounterReference(&resource));
+  }
+
+  inline void CommandBuffer::releaseResources()
+  {
+    _lockedResources.clear();
+  }
+
+  /*inline UniformMemoryPool& CommandBuffer::uniformMemoryPool() noexcept
+  {
+    return _uniformMemoryPool;
+  }
+
+  inline ScaledVolatileDescriptorPool&
+                                CommandBuffer::volatileDescriptorPool() noexcept
+  {
+    return _volatileDescriptorPool;
+  }*/
+}
---
+++ vkr/queue/CommandPool.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include <vector>
+
+#include <vulkan/vulkan.h>
+
+#include <vkr/Ref.h>
+#include <vkr/queue/CommandBuffer.h>
+
+namespace mt
+{
+  class CommandQueue;
+  class Device;
+
+  // Одновременно пул команд и буферов команд. Обертка вокруг VkCommandPool
+  // И централизованное хранилище буферов команд, которые будут постоянно
+  // переиспользоваться.
+  // Предполагает использование отдельными сессиями. В течении сессии
+  // переиспользуем или создаем(если закончились) столько буферов команд,
+  // сколько потребуется. После окончания сессии ждем, когда очередь команд
+  // израсходует все буферы и вызываем метод reset, который сбрасывает все
+  // залоченные ресурсы всех буферов и сбрасывает VkCommandPool. Пул готов
+  // к следующей сессии.
+  class CommandPool
+  {
+  private:
+    CommandPool(CommandQueue& queue);
+    CommandPool(const CommandPool&) = delete;
+    CommandPool& operator = (const CommandPool&) = delete;
+    virtual ~CommandPool() noexcept;
+
+    // Взять из пула не используемый буфер или создать новый, если свободных
+    // нет.
+    CommandBuffer& getNextBuffer();
+
+    // Сбросить пул комманд и все работающие с ним буферы команд
+    // Убедитесь, что ни один из буферов команд не находится в исполнении
+    // в очереди команд
+    void reset();
+
+  private:
+    void _cleanup() noexcept;
+
+  private:
+    VkCommandPool _handle;
+    Device& _device;
+
+    using Buffers = std::vector<Ref<CommandBuffer>>;
+    Buffers _buffers;
+    size_t _nextBuffer;
+  };
+};
