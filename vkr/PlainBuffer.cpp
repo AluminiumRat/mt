a4c20f74c64ec47737d64e3e16582a65129f6f44
Aluminium Rat <AluminiumRat@yandex.ru>
2025-10-20 21:01:04 +0300

    PlainBuffer

---
+++ vkr/PlainBuffer.cpp
@@ -0,0 +1,212 @@
+﻿#include <stdexcept>
+
+#include <util/Assert.h>
+
+#include <vkr/Device.h>
+#include <vkr/PhysicalDevice.h>
+#include <vkr/PlainBuffer.h>
+
+using namespace mt;
+
+PlainBuffer::Mapper::Mapper(PlainBuffer& buffer) :
+  _buffer(buffer),
+  _data(nullptr)
+{
+  if(vmaMapMemory(_buffer._device.allocator(),
+                  _buffer._allocation,
+                  &_data) != VK_SUCCESS)
+  {
+    throw std::runtime_error("PlainBuffer: Failed to map buffer's memory.");
+  }
+
+  if(vmaInvalidateAllocation( _buffer._device.allocator(),
+                              _buffer._allocation,
+                              0,
+                              _buffer.size()) != VK_SUCCESS)
+  {
+    throw std::runtime_error("PlainBuffer: Failed to vmaInvalidateAllocation");
+  }
+}
+
+PlainBuffer::Mapper::~Mapper()
+{
+  if(_data != nullptr)
+  {
+    if(vmaFlushAllocation(_buffer._device.allocator(),
+                          _buffer._allocation,
+                          0,
+                          _buffer.size()) != VK_SUCCESS)
+    {
+      MT_ASSERT(false && "PlainBuffer: Failed to vmaFlushAllocation");
+    }
+
+    vmaUnmapMemory(_buffer._device.allocator(), _buffer._allocation);
+  }
+}
+
+static VkBufferUsageFlags getUsageFlags(PlainBuffer::Usage usage)
+{
+  switch (usage)
+  {
+    case PlainBuffer::UPLOAD_BUFFER:
+      return VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+
+    case PlainBuffer::DOWNLOAD_BUFFER:
+      return VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+
+    case PlainBuffer::INDICES_BUFFER:
+      return  VK_BUFFER_USAGE_INDEX_BUFFER_BIT |
+              VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+
+    case PlainBuffer::UNIFORM_BUFFER:
+      return  VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT |
+              VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+
+    case PlainBuffer::VOLATILE_UNIFORM_BUFFER:
+      return  VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
+  }
+
+  MT_ASSERT(false && "PlainBuffer::getUsageFlags: unknown usage");
+};
+
+static VmaAllocationCreateInfo getVmaCreateInfo(PlainBuffer::Usage usage)
+{
+  VmaAllocationCreateInfo info{};
+
+  switch (usage)
+  {
+    case PlainBuffer::UPLOAD_BUFFER:
+    case PlainBuffer::VOLATILE_UNIFORM_BUFFER:
+      info.requiredFlags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT;
+      info.preferredFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+      break;
+
+    case PlainBuffer::DOWNLOAD_BUFFER:
+      info.requiredFlags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT;
+      info.preferredFlags = VK_MEMORY_PROPERTY_HOST_CACHED_BIT;
+      break;
+
+    case PlainBuffer::INDICES_BUFFER:
+    case PlainBuffer::UNIFORM_BUFFER:
+      info.requiredFlags = 0;
+      info.preferredFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+      break;
+
+    default:
+      MT_ASSERT(false && "PlainBuffer::getVmaCreateInfo: unknown usage");
+  }
+
+  return info;
+}
+
+PlainBuffer::PlainBuffer( Device& device,
+                          size_t size,
+                          Usage usage) :
+  _device(device),
+  _handle(VK_NULL_HANDLE),
+  _size(size),
+  _allocation(VK_NULL_HANDLE)
+{
+  try
+  {
+    VkBufferCreateInfo bufferInfo{};
+    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferInfo.size = _size;
+    bufferInfo.usage = getUsageFlags(usage);
+    bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    VmaAllocationCreateInfo allocCreateInfo = getVmaCreateInfo(usage);
+
+    if(vmaCreateBuffer( _device.allocator(),
+                        &bufferInfo,
+                        &allocCreateInfo,
+                        &_handle,
+                        &_allocation,
+                        nullptr) != VK_SUCCESS)
+    {
+      throw std::runtime_error("PlainBuffer: Failed to create buffer.");
+    }
+  }
+  catch(...)
+  {
+    _cleanup();
+    throw;
+  }
+}
+
+PlainBuffer::PlainBuffer( Device& device,
+                          size_t size,
+                          VkBufferUsageFlags bufferUsageFlags,
+                          VkMemoryPropertyFlags requiredFlags,
+                          VkMemoryPropertyFlags preferredFlags) :
+  _device(device),
+  _handle(VK_NULL_HANDLE),
+  _size(size),
+  _allocation(VK_NULL_HANDLE)
+{
+  try
+  {
+    VkBufferCreateInfo bufferInfo{};
+    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferInfo.size = _size;
+    bufferInfo.usage = bufferUsageFlags;
+    bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    VmaAllocationCreateInfo allocCreateInfo{};
+    allocCreateInfo.requiredFlags = requiredFlags;
+    allocCreateInfo.preferredFlags = preferredFlags;
+
+    if(vmaCreateBuffer( _device.allocator(),
+                        &bufferInfo,
+                        &allocCreateInfo,
+                        &_handle,
+                        &_allocation,
+                        nullptr) != VK_SUCCESS)
+    {
+      throw std::runtime_error("PlainBuffer: Failed to create buffer.");
+    }
+  }
+  catch(...)
+  {
+    _cleanup();
+    throw;
+  }
+}
+
+PlainBuffer::~PlainBuffer()
+{
+  _cleanup();
+}
+
+void PlainBuffer::_cleanup() noexcept
+{
+  if(_handle != VK_NULL_HANDLE)
+  {
+    vmaDestroyBuffer(_device.allocator(), _handle, _allocation);
+    _allocation = VK_NULL_HANDLE;
+    _handle = VK_NULL_HANDLE;
+  }
+}
+
+void PlainBuffer::uploadData(const void* data, size_t shift, size_t dataSize)
+{
+  void* bufferData;
+  if(vmaMapMemory(_device.allocator(),
+                  _allocation,
+                  &bufferData) != VK_SUCCESS)
+  {
+    throw std::runtime_error("PlainBuffer: Failed to map buffer's memory.");
+  }
+
+  memcpy((char*)bufferData + shift, data, dataSize);
+
+  if(vmaFlushAllocation(_device.allocator(),
+                        _allocation,
+                        shift,
+                        dataSize) != VK_SUCCESS)
+  {
+    MT_ASSERT(false && "PlainBuffer: Failed to vmaFlushAllocation");
+  }
+
+  vmaUnmapMemory(_device.allocator(), _allocation);
+}
---
+++ vkr/PlainBuffer.h
@@ -0,0 +1,103 @@
+﻿#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include <vk_mem_alloc.h>
+
+#include <vkr/RefCounter.h>
+
+namespace mt
+{
+  class Device;
+
+  // Просто фиксированный кусок памяти на стороне GPU
+  // Обертка вокруг VkBuffer.
+  class PlainBuffer : public RefCounter
+  {
+  public:
+    enum Usage
+    {
+      UPLOAD_BUFFER,
+      DOWNLOAD_BUFFER,
+      INDICES_BUFFER,
+      UNIFORM_BUFFER,
+      VOLATILE_UNIFORM_BUFFER
+    };
+
+    //  Маппинг буфера на ЦПУ память. используется для записи в/из ГПУ
+    //  Для загрузки данных на GPU лучше использовать PlainBuffer::uploadData,
+    //    особенно, если необходимо загрузить только чать данных.
+    //  ВНИМАНИЕ! Буфер должен быть создан с Usage UPLOAD_BUFFER или 
+    //    DOWNLOAD_BUFFER или с
+    //    requiredFlags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
+    //  ВНИМАНИЕ! Не используйте одновременно несколько мапперов на одном
+    //    и том же буфере.
+    //  ВНИМАНИЕ! Не используйте одновременно маппер и метод
+    //    PlainBuffer::uploadData на одном и том же буфере
+    class Mapper
+    {
+    public:
+      Mapper(PlainBuffer& buffer);
+      Mapper(const Mapper&) = delete;
+      Mapper& operator = (const Mapper&) = delete;
+      ~Mapper();
+
+      inline void* data() const;
+
+    private:
+      PlainBuffer& _buffer;
+      void* _data;
+    };
+
+  public:
+    // Упрощенный конструктор для типового использования
+    PlainBuffer(Device& device, size_t size, Usage usage);
+    // Полнофункциональный конструктор для кастома
+    PlainBuffer(Device& device,
+                size_t size,
+                VkBufferUsageFlags bufferUsageFlags,
+                VkMemoryPropertyFlags requiredFlags,
+                VkMemoryPropertyFlags preferredFlags);
+    PlainBuffer(const PlainBuffer&) = delete;
+    PlainBuffer& operator = (const PlainBuffer&) = delete;
+  protected:
+    virtual ~PlainBuffer();
+
+  public:
+    inline VkBuffer handle() const;
+    inline size_t size() const;
+
+    //  Загрузка данных с хоста на ГПУ.
+    //  ВНИМАНИЕ! Буфер должен быть создан с Usage UPLOAD_BUFFER или 
+    //    DOWNLOAD_BUFFER или с
+    //    requiredFlags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
+    //  ВНИМАНИЕ! Не используйте одновременно маппер и метод
+    //    PlainBuffer::uploadData на одном и том же буфере
+    void uploadData(const void* data, size_t shift, size_t dataSize);
+
+  private:
+    void _cleanup() noexcept;
+
+  private:
+    Device& _device;
+    VkBuffer _handle;
+    size_t _size;
+
+    VmaAllocation _allocation;
+  };
+
+  inline VkBuffer PlainBuffer::handle() const
+  {
+    return _handle;
+  }
+
+  inline size_t PlainBuffer::size() const
+  {
+    return _size;
+  }
+
+  inline void* PlainBuffer::Mapper::data() const
+  {
+    return _data;
+  }
+}
