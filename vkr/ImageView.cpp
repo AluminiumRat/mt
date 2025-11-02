e48a74ea68a368b61f7f33bb4d57c403cb1051af
Aluminium Rat <AluminiumRat@yandex.ru>
2025-11-02 21:16:16 +0300

    Добавил ImageView

---
+++ vkr/ImageView.cpp
@@ -0,0 +1,43 @@
+﻿#include <util/Abort.h>
+#include <vkr/ImageView.h>
+#include <vkr/Device.h>
+
+using namespace mt;
+
+ImageView::ImageView( Image& image,
+                      const ImageSlice& slice,
+                      VkImageViewType viewType,
+                      const VkComponentMapping& components) :
+  _handle(VK_NULL_HANDLE),
+  _image(&image),
+  _slice(slice),
+  _viewType(viewType),
+  _components(components)
+{
+  VkImageViewCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+  createInfo.image = image.handle();
+  createInfo.viewType = viewType;
+  createInfo.format = image.format();
+  createInfo.components = components;
+  createInfo.subresourceRange = _slice.makeRange();
+
+  if (vkCreateImageView(image.device().handle(),
+                        &createInfo,
+                        nullptr,
+                        &_handle) != VK_SUCCESS)
+  {
+    Abort("ImageView: Failed to create image view");
+  }
+}
+
+ImageView::~ImageView()
+{
+  if(_handle != VK_NULL_HANDLE)
+  {
+    vkDestroyImageView( _image->device().handle(),
+                        _handle,
+                        nullptr);
+    _handle = VK_NULL_HANDLE;
+  }
+}
---
+++ vkr/ImageView.h
@@ -0,0 +1,79 @@
+﻿#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include <vkr/Image.h>
+#include <vkr/ImageSlice.h>
+#include <vkr/RefCounter.h>
+#include <vkr/Ref.h>
+
+namespace mt
+{
+  // Обертка вокруг VkImageView
+  class ImageView : public RefCounter
+  {
+  public:
+    static constexpr VkComponentMapping defaultComponentMapping =
+                                                { .r = VK_COMPONENT_SWIZZLE_R,
+                                                  .g = VK_COMPONENT_SWIZZLE_G,
+                                                  .b = VK_COMPONENT_SWIZZLE_B,
+                                                  .a = VK_COMPONENT_SWIZZLE_A};
+  public:
+    ImageView(Image& image,
+              const ImageSlice& slice,
+              VkImageViewType viewType,
+              const VkComponentMapping& components = defaultComponentMapping);
+    ImageView(const ImageView&) = delete;
+    ImageView& operator = (const ImageView&) = delete;
+  protected:
+    virtual ~ImageView();
+
+  public:
+    inline VkImageView handle() const noexcept;
+    inline Image& image() const noexcept;
+    const ImageSlice& slice() const noexcept;
+    inline VkImageViewType viewType() const noexcept;
+    inline const VkComponentMapping& components() const noexcept;
+
+    // Разрешение произвольного mip уровня. Отчет начинается с первого уровня,
+    // попавшего в ImageView
+    inline glm::uvec3 extent(uint32_t mipLevel = 0) const noexcept;
+
+  private:
+    VkImageView _handle;
+    Ref<Image> _image;
+    ImageSlice _slice;
+    VkImageViewType _viewType;
+    VkComponentMapping _components;
+  };
+
+  inline VkImageView ImageView::handle() const noexcept
+  {
+    return _handle;
+  }
+
+  inline Image& ImageView::image() const noexcept
+  {
+    return *_image;
+  }
+
+  inline const ImageSlice& ImageView::slice() const noexcept
+  {
+    return _slice;
+  }
+
+  inline VkImageViewType ImageView::viewType() const noexcept
+  {
+    return _viewType;
+  }
+
+  inline const VkComponentMapping& ImageView::components() const noexcept
+  {
+    return _components;
+  }
+
+  inline glm::uvec3 ImageView::extent(uint32_t mipLevel) const noexcept
+  {
+    return _image->extent(_slice.baseMipLevel() + mipLevel);
+  }
+}
