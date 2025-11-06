b27b598f246b01b380bf40b50d72a0174c98f482
Aluminium Rat <AluminiumRat@yandex.ru>
2025-11-06 17:58:17 +0300

    Добавил AbstractPipeline

---
+++ vkr/pipeline/AbstractPipeline.cpp
@@ -0,0 +1,50 @@
+﻿#include <util/Assert.h>
+#include <vkr/pipeline/AbstractPipeline.h>
+#include <vkr/pipeline/ShaderModule.h>
+#include <vkr/Device.h>
+
+using namespace mt;
+
+AbstractPipeline::AbstractPipeline(Device& device, VkPipeline handle) :
+  _device(device),
+  _handle(handle)
+{
+  MT_ASSERT(_handle != VK_NULL_HANDLE);
+}
+
+AbstractPipeline::VkShadersInfo AbstractPipeline::createVkShadersInfo(
+                                            std::span<const ShaderInfo> shaders)
+{
+  MT_ASSERT(!shaders.empty());
+
+  std::vector<VkPipelineShaderStageCreateInfo> vkShadersInfo;
+  vkShadersInfo.reserve(shaders.size());
+
+  for(const ShaderInfo& shader : shaders)
+  {
+    MT_ASSERT(shader.module != nullptr);
+    MT_ASSERT(!shader.entryPoint.empty());
+
+    VkPipelineShaderStageCreateInfo vkShaderInfo{};
+    vkShaderInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vkShaderInfo.stage = shader.stage;
+    vkShaderInfo.module = shader.module->handle();
+    vkShaderInfo.pName = shader.entryPoint.c_str();
+  }
+
+  return vkShadersInfo;
+}
+
+AbstractPipeline::~AbstractPipeline() noexcept
+{
+  cleanup();
+}
+
+void AbstractPipeline::cleanup() noexcept
+{
+  if(_handle != VK_NULL_HANDLE)
+  {
+    vkDestroyPipeline(_device.handle(), _handle, nullptr);
+    _handle = VK_NULL_HANDLE;
+  }
+}
---
+++ vkr/pipeline/AbstractPipeline.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include <span>
+#include <string>
+#include <vector>
+
+#include <vulkan/vulkan.h>
+
+#include <vkr/RefCounter.h>
+
+namespace mt
+{
+  class Device;
+  class ShaderModule;
+
+  //  Базовый класс для GraphicPipeline и ComputePipeline
+  //  Обертка вокруг VkPipeline, общий код для графического и компьют конвеера
+  class AbstractPipeline : public RefCounter
+  {
+  public:
+    // Информация о шейдерных стадиях пайплайна
+    struct ShaderInfo
+    {
+      const ShaderModule* module;     // Не должен быть nullptr
+      VkShaderStageFlagBits stage;    // Должен указывать строго на 1 стадию
+      std::string entryPoint;
+    };
+
+  public:
+    AbstractPipeline(Device& device, VkPipeline handle);
+    AbstractPipeline(const AbstractPipeline&) = delete;
+    AbstractPipeline& operator = (const AbstractPipeline&) = delete;
+    virtual ~AbstractPipeline() noexcept;
+  public:
+
+    inline VkPipeline handle() const noexcept;
+
+  protected:
+    using VkShadersInfo = std::vector<VkPipelineShaderStageCreateInfo>;
+    static VkShadersInfo createVkShadersInfo(
+                                            std::span<const ShaderInfo> shaders);
+    virtual void cleanup() noexcept;
+
+  private:
+    Device& _device;
+    VkPipeline _handle;
+  };
+
+  inline VkPipeline AbstractPipeline::handle() const noexcept
+  {
+    return _handle;
+  }
+}
