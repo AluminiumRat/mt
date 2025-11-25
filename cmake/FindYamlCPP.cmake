1e2c7f6c7de044c319e5ad60ad5e7210829d9013
Aluminium Rat <AluminiumRat@yandex.ru>
2025-11-25 18:10:20 +0300

    Добавил Yaml-cpp в third_party

---
+++ cmake/FindYamlCPP.cmake
@@ -0,0 +1,26 @@
+find_path(YamlCPP_INCLUDE_DIRS
+          yaml-cpp/yaml.h
+          HINTS "third_party/yaml-cpp/include"
+          REQUIRED)
+
+if(YamlCPP_INCLUDE_DIRS)
+  find_library( YamlCPP_LIBRARY_DEBUG
+                NAMES yaml-cppd
+                HINTS "third_party/yaml-cpp/build"
+                REQUIRED)
+
+  find_library( YamlCPP_LIBRARY_RELEASE
+                NAMES yaml-cpp
+                HINTS "third_party/yaml-cpp/build"
+                REQUIRED)
+
+  mark_as_advanced( YamlCPP_INCLUDE_DIRS
+                    YamlCPP_LIBRARY_DEBUG
+                    YamlCPP_LIBRARY_RELEASE)
+
+  include(SelectLibraryConfigurations)
+  select_library_configurations(YamlCPP)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(YamlCPP DEFAULT_MSG YamlCPP_LIBRARIES YamlCPP_INCLUDE_DIRS)
