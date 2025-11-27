ba03d1493638bed34dee2d75e140bf57282e3ccf
Aluminium Rat <AluminiumRat@yandex.ru>
2025-11-27 11:48:00 +0300

    Добавил dumpHardware в экзамплы

---
+++ examples/1_0_dumpHardware/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <exception>
+
+#include <vkr/dumpHardware.h>
+#include <vkr/VKRLib.h>
+
+using namespace mt;
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    VKRLib vkrLib("VKTest",
+                  VKRLib::AppVersion{.major = 0, .minor = 0, .patch = 0},
+                  VK_API_VERSION_1_3,
+                  true,
+                  true);
+
+    dumpHardware( true,       // dumpLayers
+                  true,       // dumpExtensions
+                  true,       // dumpDevices
+                  true,       // dumpQueues
+                  true,       // dumpMemory
+                  nullptr);
+    return 0;
+  }
+  catch (std::exception& error)
+  {
+    Log::error() << error.what();
+    return 1;
+  }
+}
