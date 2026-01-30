90455a9d736da4cac6201b686732cbcdd4579ae4
Aluminium Rat <AluminiumRat@yandex.ru>
2026-01-30 03:08:46 +0300

    Добавил пример 2_2_technique_compute

---
+++ app/examples/2_2_technique_compute/TestWindow.cpp
@@ -0,0 +1,137 @@
+﻿#include <technique/TechniqueLoader.h>
+#include <vkr/queue/CommandProducerGraphic.h>
+#include <vkr/Device.h>
+#include <TestWindow.h>
+
+using namespace mt;
+
+TestWindow::TestWindow(Device& device) :
+  RenderWindow( device,
+                "Test window",
+                std::nullopt,
+                std::nullopt,
+                VK_FORMAT_UNDEFINED),
+  _computeTechnique{
+    .configurator{new TechniqueConfigurator(device, "Compute technique")},
+    .technique{*_computeTechnique.configurator},
+    .pass{_computeTechnique.technique.getOrCreatePass("ComputePass")},
+    .texture{_computeTechnique.technique.getOrCreateResourceBinding(
+                                                            "storageTexture")}},
+  _renderTechnique{ 
+    .configurator{new TechniqueConfigurator(device, "Render technique")},
+    .technique{*_renderTechnique.configurator},
+    .pass{_renderTechnique.technique.getOrCreatePass("RenderPass")},
+    .colorSelector{_renderTechnique.technique.getOrCreateSelection(
+                                                              "colorSelector")},
+    .vertexBuffer{_renderTechnique.technique.getOrCreateResourceBinding(
+                                                                  "vertices")},
+    .texture{_renderTechnique.technique.getOrCreateResourceBinding(
+                                                              "colorTexture")},
+    .color{_renderTechnique.technique.getOrCreateUniform("colorData.color")}}
+{
+  loadConfigurator( *_computeTechnique.configurator,
+                    "examples/compute/compute.tch");
+  _computeTechnique.configurator->rebuildConfiguration();
+
+  loadConfigurator( *_renderTechnique.configurator,
+                    "examples/technique/technique.tch");
+  _renderTechnique.configurator->rebuildConfiguration();
+
+  _renderTechnique.colorSelector.setValue("1");
+  _renderTechnique.color.setValue(glm::vec4(1));
+
+  _createVertexBuffer();
+  _createTexture();
+}
+
+void TestWindow::_createVertexBuffer()
+{
+  struct Vertex
+  {
+    alignas(16) glm::vec3 position;
+    alignas(16) glm::vec4 color;
+  };
+  Vertex vertices[3] =
+                    { {.position = {0.0f, -0.5f, 0.0f}, .color = {1, 0, 0, 1}},
+                      {.position = {0.5f, 0.5f, 0.0f}, .color = {0, 1, 0, 1}},
+                      {.position = {-0.5f, 0.5f, 0.0f}, .color = {0, 0, 1, 1}}};
+  Ref<DataBuffer> buffer(new DataBuffer(device(),
+                                        sizeof(vertices),
+                                        DataBuffer::STORAGE_BUFFER,
+                                        "Vertex buffer"));
+  device().graphicQueue()->uploadToBuffer(*buffer,
+                                          0,
+                                          sizeof(vertices),
+                                          vertices);
+  _renderTechnique.vertexBuffer.setBuffer(buffer);
+}
+
+void TestWindow::_createTexture()
+{
+  _renderedImage = new Image( device(),
+                              VK_IMAGE_TYPE_2D,
+                              VK_IMAGE_USAGE_SAMPLED_BIT |
+                                VK_IMAGE_USAGE_STORAGE_BIT,
+                              0,
+                              VK_FORMAT_R8G8B8A8_UNORM,
+                              glm::uvec3(256, 256, 1),
+                              VK_SAMPLE_COUNT_1_BIT,
+                              1,
+                              1,
+                              false,
+                              "Test image");
+
+  Ref<ImageView> imageView(new ImageView( *_renderedImage,
+                                          ImageSlice(*_renderedImage),
+                                          VK_IMAGE_VIEW_TYPE_2D));
+  _renderTechnique.texture.setImage(imageView);
+  _computeTechnique.texture.setImage(imageView);
+}
+
+void TestWindow::drawImplementation(FrameBuffer& frameBuffer)
+{
+  std::unique_ptr<CommandProducerGraphic> commandProducer =
+                                      device().graphicQueue()->startCommands();
+
+  commandProducer->imageBarrier(*_renderedImage,
+                                ImageSlice(*_renderedImage),
+                                VK_IMAGE_LAYOUT_UNDEFINED,
+                                VK_IMAGE_LAYOUT_GENERAL,
+                                0,
+                                0,
+                                0,
+                                0);
+
+  //  Заполнение текстуры через компьют
+  Technique::BindCompute bindCompute( _computeTechnique.technique,
+                                      _computeTechnique.pass,
+                                      *commandProducer);
+  if (bindCompute.isValid())
+  {
+    commandProducer->dispatch(32, 32, 1);
+    bindCompute.release();
+  }
+
+  commandProducer->imageBarrier(*_renderedImage,
+                                ImageSlice(*_renderedImage),
+                                VK_IMAGE_LAYOUT_GENERAL,
+                                VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+                                VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT,
+                                VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
+                                VK_ACCESS_SHADER_WRITE_BIT,
+                                VK_ACCESS_SHADER_READ_BIT);
+
+  //  Отрисовка
+  CommandProducerGraphic::RenderPass renderPass(*commandProducer, frameBuffer);
+    Technique::BindGraphic bindRender(_renderTechnique.technique,
+                                      _renderTechnique.pass,
+                                      *commandProducer);
+    if (bindRender.isValid())
+    {
+      commandProducer->draw(3);
+      bindRender.release();
+    }
+  renderPass.endPass();
+
+  device().graphicQueue()->submitCommands(std::move(commandProducer));
+}
---
+++ app/examples/2_2_technique_compute/TestWindow.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+#include <gui/RenderWindow.h>
+#include <technique/Technique.h>
+#include <util/Ref.h>
+#include <vkr/image/ImageView.h>
+#include <vkr/DataBuffer.h>
+#include <vkr/Sampler.h>
+
+namespace mt
+{
+  class TestWindow : public RenderWindow
+  {
+  public:
+    TestWindow(Device& device);
+    TestWindow(const TestWindow&) = delete;
+    TestWindow& operator = (const TestWindow&) = delete;
+    virtual ~TestWindow() noexcept = default;
+
+  protected:
+    virtual void drawImplementation(FrameBuffer& frameBuffer) override;
+
+  private:
+    void _createVertexBuffer();
+    void _createTexture();
+
+  private:
+    Ref<Image> _renderedImage;
+
+    struct ComputeTechnique
+    {
+      Ref<TechniqueConfigurator> configurator;
+      Technique technique;
+      TechniquePass& pass;
+      ResourceBinding& texture;
+    };
+    ComputeTechnique _computeTechnique;
+
+    struct RenderTechnique
+    {
+      Ref<TechniqueConfigurator> configurator;
+      Technique technique;
+      TechniquePass& pass;
+
+      Selection& colorSelector;
+
+      ResourceBinding& vertexBuffer;
+      ResourceBinding& texture;
+
+      UniformVariable& color;
+    };
+    RenderTechnique _renderTechnique;
+  };
+}
