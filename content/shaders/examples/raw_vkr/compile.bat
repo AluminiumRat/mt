952fdaac40017fc61a81cfc20f9b29237b6667a9
aluminiumrat <aluminiumrat@yandex.ru>
2026-02-02 14:34:51 +0300

    Правки по ручной сборке шейдеров

--- content/shaders/examples/raw_vkr/compile.bat
+++ content/shaders/examples/raw_vkr/compile.bat
@@ -1,2 +1,2 @@
-C:/VulkanSDK/1.4.321.1/Bin/glslc.exe shader.vert -o shader.vert.spv
-C:/VulkanSDK/1.4.321.1/Bin/glslc.exe shader.frag -o shader.frag.spv
+%VULKAN_SDK%/Bin/glslc.exe shader.vert -o shader.vert.spv
+%VULKAN_SDK%/Bin/glslc.exe shader.frag -o shader.frag.spv
