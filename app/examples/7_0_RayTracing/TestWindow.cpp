335d6ea940715c47d59221565cedd463c4791154
aluminiumrat <aluminiumrat@yandex.ru>
2026-03-04 17:27:54 +0300

    Добавил заготовку под тестовый проект 7_0_RayTracing

---
+++ app/examples/7_0_RayTracing/TestWindow.cpp
@@ -0,0 +1,16 @@
+﻿#include <TestWindow.h>
+
+using namespace mt;
+
+TestWindow::TestWindow(Device& device) :
+  RenderWindow( device,
+                "Test window",
+                std::nullopt,
+                std::nullopt,
+                VK_FORMAT_UNDEFINED)
+{
+}
+
+void TestWindow::drawImplementation(FrameBuffer& frameBuffer)
+{
+}
---
+++ app/examples/7_0_RayTracing/TestWindow.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <gui/RenderWindow.h>
+
+namespace mt
+{
+  class TestWindow : public RenderWindow
+  {
+  public:
+    TestWindow(Device& device);
+    TestWindow(const TestWindow&) = delete;
+    TestWindow& operator = (const TestWindow&) = delete;
+    virtual ~TestWindow() noexcept = default;
+
+  protected:
+    virtual void drawImplementation(FrameBuffer& frameBuffer) override;
+  };
+}
